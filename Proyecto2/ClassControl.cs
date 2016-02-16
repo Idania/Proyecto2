6f2230c87ce47a24d9f400857f0c3a62d4e2da34
Idania <idania.gonz.reyes@gmail.com>
2016-02-15 21:21:31 -0600

    Proc
    Agregar Archivos de Texto

---
+++ Proyecto2/ClassControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace Proyecto2
+{
+  public   class ClassControl
+    {
+
+     
+      public string nom { set; get; }
+
+      public ClassControl(){
+
+        
+      
+      }
+      public void CrearArchivo() {
+
+
+          File.Create(nom).Close();
+          MessageBox.Show("Archivo creado");
+          
+          
+      
+      }//fin metodo crear archivo
+
+      public void AgregarDatos(ref string mens) {
+
+          StreamWriter Arch = new StreamWriter(nom);
+
+          Arch.WriteLine(mens);
+          MessageBox.Show("Mensaje Agregado");
+          Arch.Close();
+      
+          
+      
+      }
+
+      public void Mostrar(ListBox lista) {
+          lista.Items.Clear();
+          StreamReader Arch = new StreamReader(nom);
+
+
+          lista.Items.Add(Arch.ReadLine());
+          Arch.Close();
+      
+      }
+
+  
+  }// fin class
+}// fin namespace
---
+++ Proyecto2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Proyecto2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        ClassControl C1 = new ClassControl();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            C1.nom = textBox1.Text;
+            C1.CrearArchivo();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string me = textBox2.Text;
+            C1.AgregarDatos(ref me);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            C1.Mostrar(listBox1);
+        }
+        }
+}
